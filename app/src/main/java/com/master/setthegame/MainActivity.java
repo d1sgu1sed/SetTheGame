d4f5505149ab7402d7af9be2731321e3e0494896
Ivan Kichigin <ivan.kichigin977@gamil.com>
2023-06-27 21:50:46 +0800

    Добавлена вся обработка основной активности

--- app/src/main/java/com/master/setthegame/MainActivity.java
+++ app/src/main/java/com/master/setthegame/MainActivity.java
@@ -2,7 +2,15 @@ package com.master.setthegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import org.json.JSONException;
 
@@ -14,11 +22,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText nicknameView = findViewById(R.id.nickname);
+        EditText passwordView = findViewById(R.id.password);
+        ImageView rulesButton = findViewById(R.id.ruleBut);
+        ImageView playButton = findViewById(R.id.playBut);
+        ImageView exitButton = findViewById(R.id.exitBut);
+
         Controller controller = new Controller();
-        try {
-            controller.register("Andrey", "strongpassword1337");
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
-        }
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nickname = nicknameView.getText().toString();
+                String password = passwordView.getText().toString();
+                if(!nickname.replaceAll(" ", "").equals("") &&
+                        !password.replaceAll(" ", "").equals("")){
+                    try {
+                        boolean req = controller.register(nickname, password);
+                        if (!req)
+                            showErrorMessage("Введеные неверные логин или пароль!");
+                        //TODO: Переход на активность игры
+                        /*else{
+                            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                            String token = controller.getToken();
+                            intent.putExtra("token", token);
+                            startActivity(intent);
+                        }*/
+                    } catch (IOException | JSONException e) {
+                        showErrorMessage("Ошибка работы сервера!");
+                    }
+
+                }
+                else{
+                    showErrorMessage("Введите данные!");
+                }
+            }
+        });
+
+        rulesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Переход на активность правил
+                /*Intent intent = new Intent(MainActivity.this, RulesActivity.class);
+                startActivity(intent);*/
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
     }
+
+    private void showErrorMessage(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Ошибка!")
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("ОК",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
 }
